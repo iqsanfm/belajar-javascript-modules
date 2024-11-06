ec4662ad2a375b1bf55635221f41d4059baaebbb
iqsanfm <iqsanfm@gmail.com>
2024-11-06 12:34:19 +0700

    Javascript | Modules Day 2

---
+++ scripts/default-dan-named.js
@@ -0,0 +1,8 @@
+export const title = "Belajar Javascript Module";
+
+const content = "Content belajar javascript module";
+
+const author = "Iqsan Faisal";
+
+export { content };
+export default author;
---
+++ scripts/module-object.js
@@ -0,0 +1,12 @@
+export const firstName = "Iqsan";
+export const lastName = "Faisal";
+
+export function hello() {
+  console.log(`Hello from Iqsan`);
+}
+
+export class Person {
+  constructor() {
+    this.name = "Iqsan";
+  }
+}
